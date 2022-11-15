a1854071fdd8323b99e2c1e162dba17168bab694
salmanferrum <salman.haider@ferrum.network>
2022-11-15 18:58:51 +0500

    IonicSwapv1

--- scripts/deployRouter.js
+++ scripts/deployRouter.js
@@ -1,24 +1,22 @@
-const { ethers, upgrades } = require('hardhat');
+const { ethers, upgrades } = require("hardhat");
 
 async function main() {
-
-
   const FiberRouter = await ethers.getContractFactory("FiberRouter");
-  console.log('Deploying Router...');
+  console.log("Deploying Router...");
 
-  const fiberRouter = await upgrades.deployProxy(FiberRouter, { initializer: 'initialize' })
+  const fiberRouter = await upgrades.deployProxy(FiberRouter, {
+    initializer: "initialize",
+  });
   await fiberRouter.deployed();
-
 
   console.log(`Router deployed to ${fiberRouter.address}`);
 
-  if(network.name == 'hardhat') return
-  await fiberRouter.deployTransaction.wait(21)
-  console.log('Verifing...')
+  if (network.name == "hardhat") return;
+  await fiberRouter.deployTransaction.wait(21);
+  console.log("Verifing...");
   await hre.run("verify:verify", {
     address: fiberRouter.address,
-    constructorArguments: [
-    ],
+    constructorArguments: [],
   });
   console.log("Contract verified successfully !");
 }
