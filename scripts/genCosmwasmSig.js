df550aafb7cb501cb96fed83d4a24db476c15fcd
jpine3528 <jpine3528@gmail.com>
2022-12-26 09:08:21 +0800

    Add notes for withdraw signed signature generation and add ethers package

--- scripts/genCosmwasmSig.js
+++ scripts/genCosmwasmSig.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Wallet, utils, providers } = require("ethers");
 
 async function main() {
+  // Note: when generating message on ferrum node, the order of fields should be kept.
   const messageText =
     '{"chain_id":"chain_id","payee":"payee","token":"token","amount":"10000","salt":"salt"}';
 
@@ -9,6 +10,7 @@ async function main() {
   let privKey = process.env.PRIVATE_KEY;
   const wallet = new Wallet(privKey, provider);
   const signature = await wallet.signMessage(messageText);
+  // Note: signature should not have 0x prefix when it's provided to withdraw signed command
   console.log("signature", signature);
   console.log("address", wallet.address);
 }
