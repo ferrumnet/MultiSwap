d70826d2c1409af98687468bcc2f504cb6c7c40b
sibghatullah1997 <h.m.sibghatullah1997@gmail.com>
2023-04-26 18:47:28 +0500

    test cases added and their dependencies adjusted

--- contracts/token/Token.sol
+++ contracts/token/Token.sol
@@ -6,11 +6,16 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
-contract Token is ERC20Upgradeable {
+contract MockToken is ERC20Upgradeable {
 
     //initialize function is constructor for upgradeable smart contract 
     function initialize(string memory _name, string memory _symbol, uint256 initialSupply) public initializer {
         __ERC20_init(_name, _symbol);
         _mint(msg.sender, initialSupply);
     }
+
+    function mint(address beneficiary, uint256 amount) external {
+        _mint(beneficiary, amount);
+    }
+
 }
