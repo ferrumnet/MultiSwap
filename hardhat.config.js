5b90a8ed5991c56f8fa2efe9f3f7535af0e46446
sibghatullah1997 <h.m.sibghatullah1997@gmail.com>
2023-10-30 18:13:25 +0500

    Fee Manager Implemented as we can detect a specific amount from swap token

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,49 +1,12 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
-require('solidity-coverage')
 const { ethers } = require("ethers");
-require("dotenv").config();
+require('dotenv').config()
 
 module.exports = {
   solidity: {
     compilers: [
-      {
-        version: "0.6.6",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20,
-          },
-        },
-      },
-      {
-        version: "0.5.16",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20,
-          },
-        },
-      },
-      {
-        version: "0.7.3",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20,
-          },
-        },
-      },
-      {
-        version: "0.8.0",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 20,
-          },
-        },
-      },
       {
         version: "0.8.2",
         settings: {
@@ -54,44 +17,54 @@ module.exports = {
         },
       },
       {
-        version: "0.4.18",
+        version: "0.8.17",
         settings: {
           optimizer: {
-            // enabled: true,
-            // runs: 200,
+            enabled: true,
+            runs: 200,
           },
         },
       },
-    ]
+
+    ],
   },
   networks: {
-    hardhat: {
-      forking: {
-        url: `https://goerli.blockpi.network/v1/rpc/public`,
-
-      },
-      // url: "http://localhost:8545", // default Hardhat Network URL
-      chainId: 31337,
-    },
     goerli: {
-      url: `https://nd-018-780-500.p2pify.com/8d55fdf55750fe8f435ef82b610d1bba`,
-      accounts: [process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1],
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY0],
     },
-    ethereum: {
-      url: `https://nd-611-696-948.p2pify.com/8a54d0bc389e645253087fd1a6c5fe3a`,
-      accounts: [process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1],
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY0]
+    },
+    polygon: {
+      url: `https://polygon-rpc.com`,
+      accounts: [process.env.PRIVATE_KEY0]
     },
     binance: {
-      url: `https://nd-409-138-440.p2pify.com/a2b2f87cd496703b1cc64ff8e91b7981`,
-      accounts: [process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1],
+      url: "https://nd-200-273-656.p2pify.com/3e83315b2e4a4b3d7bd838f8a89ca96c",
+      accounts: [process.env.PRIVATE_KEY0]
     },
-    neonTestnet: {
-      url: `https://devnet.neonevm.org`,
-      accounts: [process.env.PRIVATE_KEY0, process.env.PRIVATE_KEY1],
+    kava: {
+      url: 'https://evm2.kava.io',
+      accounts: [process.env.PRIVATE_KEY0]
     },
-  },
-  etherscan: {
-    apiKey: process.env.BSCSCAN_API_KEY,
+    AvalancheMainnet: {
+      url: 'https://nd-118-315-546.p2pify.com/048dd2e7493f4804ffed70b2acfffe8b/ext/bc/C/rpc',
+      accounts: [process.env.PRIVATE_KEY0]
+    },
+    optimismMainnet: {
+      url: 'https://mainnet.optimism.io',
+      accounts: [process.env.PRIVATE_KEY0]
+    },
+    ethereum: {
+      url: 'https://nd-611-696-948.p2pify.com/8a54d0bc389e645253087fd1a6c5fe3a',
+      accounts: [process.env.PRIVATE_KEY0]
+    },
+    arbitrum: {
+      url: 'https://nd-829-997-700.p2pify.com/790712c620e64556719c7c9f19ef56e3',
+      accounts: [process.env.PRIVATE_KEY0]
+    }
   },
   etherscan: {
     apiKey: process.env.POLYGONSCAN_API_KEY,
