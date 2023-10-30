24f75518a9bf14d4e9b059466658680db034c58d
sibghatullah1997 <h.m.sibghatullah1997@gmail.com>
2023-10-30 19:08:40 +0500

    SourceBridgeAmount added for event listening at the end of all swaps functions as well as oneInch integration done on swap and withdraw

--- contracts/upgradeable-Bridge/FiberRouter.sol
+++ contracts/upgradeable-Bridge/FiberRouter.sol
@@ -25,7 +25,8 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
         uint256 targetChainId,
         uint256 sourceAmount,
         address sourceAddress,
-        address targetAddress
+        address targetAddress,
+        uint256 swapBridgeAmount
     );
 
     event Withdraw(
@@ -110,7 +111,8 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
         uint256 amount,
         uint256 targetNetwork,
         address targetToken,
-        address targetAddress
+        address targetAddress,
+        uint256 swapBridgeAmount
     ) external {
         IERC20Upgradeable(token).safeTransferFrom(
             msg.sender,
@@ -132,7 +134,8 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
             targetNetwork,
             amount,
             _msgSender(),
-            targetAddress
+            targetAddress,
+            swapBridgeAmount
         );
     }
 
@@ -188,6 +191,61 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
        will also be required on the other end
      @param crossTargetAddress The target address for the swap
      */
+    function swapAndCrossOneInch(
+        address swapRouter,
+        uint256 amountIn,
+        uint256 amountCrossMin, // amountOutMin on uniswap
+        uint256 crossTargetNetwork,
+        address crossTargetToken,
+        address crossTargetAddress,
+        uint256 swapBridgeAmount,
+        bytes memory _calldata,
+        address fromToken,
+        address foundryToken
+    ) external nonReentrant {
+        amountIn = SafeAmount.safeTransferFrom(
+            fromToken,
+            msg.sender,
+            address(this),
+            amountIn
+        );
+        IERC20Upgradeable(fromToken).approve(swapRouter, amountIn);
+        _swapAndCrossOneInch(
+            msg.sender,
+            swapRouter,
+            amountIn,
+            amountCrossMin,
+            crossTargetNetwork,
+            crossTargetToken,
+            _calldata,
+            fromToken,
+            foundryToken
+        );
+        emit Swap(
+            fromToken,
+            crossTargetToken,
+            block.chainid,
+            crossTargetNetwork,
+            amountIn,
+            _msgSender(),
+            crossTargetAddress,
+            swapBridgeAmount
+        );
+    }
+
+
+    /*
+     @notice Do a local swap and generate a cross-chain swap
+     @param swapRouter The local swap router
+     @param amountIn The amount in
+     @param amountCrossMin Equivalent to amountOutMin on uniswap
+     @param path The swap path
+     @param deadline The swap dealine
+     @param crossTargetNetwork The target network for the swap
+     @param crossSwapTargetTokenTo If different than crossTargetToken, a swap
+       will also be required on the other end
+     @param crossTargetAddress The target address for the swap
+     */
     function swapAndCross(
         address swapRouter,
         uint256 amountIn,
@@ -196,7 +254,8 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
         uint256 deadline,
         uint256 crossTargetNetwork,
         address crossTargetToken,
-        address crossTargetAddress
+        address crossTargetAddress,
+        uint256 swapBridgeAmount
     ) external nonReentrant {
         amountIn = SafeAmount.safeTransferFrom(
             path[0],
@@ -222,7 +281,8 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
             crossTargetNetwork,
             amountIn,
             _msgSender(),
-            crossTargetAddress
+            crossTargetAddress,
+            swapBridgeAmount
         );
     }
 
@@ -277,52 +337,6 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
     }
 
     /*
-     @notice Do a local swap and generate a cross-chain swap
-     @param swapRouter The local swap router
-     @param amountCrossMin Equivalent to amountOutMin on uniswap
-     @param path The swap path
-     @param deadline The swap dealine
-     @param crossTargetNetwork The target network for the swap
-     @param crossSwapTargetTokenTo If different than crossTargetToken, a swap
-       will also be required on the other end
-     @param crossTargetAddress The target address for the swap
-     */
-    function swapAndCrossETH(
-        address swapRouter,
-        uint256 amountCrossMin, // amountOutMin
-        address[] calldata path,
-        uint256 deadline,
-        uint256 crossTargetNetwork,
-        address crossTargetToken,
-        address crossTargetAddress
-    ) external payable nonReentrant {
-        uint256 amountIn = msg.value;
-        address weth = IUniswapV2Router01(swapRouter).WETH();
-        // approveIfRequired(weth, swapRouter, amountIn);
-        IERC20Upgradeable(weth).approve(swapRouter, amountIn);
-        IWETH(weth).deposit{value: amountIn}();
-        _swapAndCross(
-            msg.sender,
-            swapRouter,
-            amountIn,
-            amountCrossMin,
-            path,
-            deadline,
-            crossTargetNetwork,
-            crossTargetToken
-        );
-        emit Swap(
-            path[0],
-            crossTargetToken,
-            block.chainid,
-            crossTargetNetwork,
-            amountIn,
-            _msgSender(),
-            crossTargetAddress
-        );
-    }
-
-    /*
      @notice Withdraws funds based on a multisig
      @dev For signature swapToToken must be the same as token
      @param token The token to withdraw
@@ -348,37 +362,6 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
         );
 
         emit Withdraw(token, payee, amount, salt, multiSignature);
-    }
-
-    /*
-     @notice Withdraw funds based on a multisig
-     @dev For signature swapToToken must be the same as token
-     @param token The token to withdraw
-     @param payee Address for where to send the tokens to
-     @param amount The amount
-     @param sourceChainId The source chain initiating the tx
-     @param swapTxId The txId for the swap from the source chain
-     @param multiSignature The multisig validator signature
-     */
-    function withdrawSignedAndSwapToFoundry(
-        address bridgeFoundry,
-        address targetFoundry,
-        address payee,
-        uint256 amount,
-        bytes32 salt,
-        bytes memory multiSignature
-    ) external nonReentrant {
-        uint256 bridgeFoundryPrice = getFoundryTokenPrice(bridgeFoundry);
-        uint256 targetFoundryPrice = getFoundryTokenPrice(targetFoundry);
-        uint256 amountOut = (amount * bridgeFoundryPrice) / targetFoundryPrice;
-        FundManager(pool).withdrawSigned(
-            targetFoundry,
-            payee,
-            amountOut,
-            salt,
-            multiSignature
-        );
-        emit Withdraw(targetFoundry, payee, amount, salt, multiSignature);
     }
 
     /*
@@ -442,34 +425,35 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
      @param deadline The swap deadline
      @param multiSignature The multisig validator signature
      */
-    function withdrawSignedAndSwapETH(
+    function withdrawSignedAndSwapOneInch(
         address to,
         address swapRouter,
         uint256 amountIn,
-        uint256 amountOutMin,
-        address[] calldata path,
-        uint256 deadline,
+        uint256 amountOutMin, // amountOutMin on uniswap
+        address foundryToken,
+        bytes memory _calldata,
         bytes32 salt,
         bytes memory multiSignature
     ) external {
+        require(foundryToken != address(0), "Bad Token Address");
+
         FundManager(pool).withdrawSigned(
-            path[0],
+            foundryToken,
             address(this),
             amountIn,
             salt,
             multiSignature
         );
-        amountIn = IERC20Upgradeable(path[0]).balanceOf(address(this)); // Actual amount received
-        IUniswapV2Router02(swapRouter)
-            .swapExactTokensForETHSupportingFeeOnTransferTokens(
-                amountIn,
-                amountOutMin,
-                path,
-                to,
-                deadline
-            );
+        amountIn = IERC20Upgradeable(foundryToken).balanceOf(address(this)); // Actual amount received
+        IERC20Upgradeable(foundryToken).approve(swapRouter, amountIn);
+        (bool success, ) = address(swapRouter).call(_calldata);
+
+        if (!success) {
+            revert('SWAP_FAILED');
+        }
+        IERC20Upgradeable(foundryToken).transfer(to, amountOutMin);
         emit Withdraw(
-            path[path.length - 1],
+            foundryToken,
             to,
             amountOutMin,
             salt,
@@ -512,6 +496,44 @@ contract FiberRouter is ReentrancyGuardUpgradeable, OwnableUpgradeable {
         IERC20Upgradeable(crossToken).approve(pool, amountCrossMin);
         FundManager(pool).swapToAddress(
             crossToken,
+            amountCrossMin,
+            crossTargetNetwork,
+            crossTargetToken,
+            to
+        );
+    }
+
+    /*
+    @notice Runs a local swap and then a cross chain swap
+    @param to The receiver
+    @param swapRouter the swap router
+    @param amountIn The amount in
+    @param amountCrossMin Equivalent to amountOutMin on uniswap 
+    @param path The swap path
+    @param deadline The swap deadline
+    @param crossTargetNetwork The target chain ID
+    @param crossTargetToken The target network token
+    @param crossSwapTargetTokenTo The target network token after swap
+    @param crossTargetAddress The receiver of tokens on the target network
+    */
+    function _swapAndCrossOneInch(
+        address to,
+        address swapRouter,
+        uint256 amountIn,
+        uint256 amountCrossMin,
+        uint256 crossTargetNetwork,
+        address crossTargetToken, // address crossSwapTargetTokenTo // address crossTargetAddress
+        bytes memory _calldata,
+        address fromToken,
+        address foundryToken
+    ) internal {
+        (bool success, ) = address(swapRouter).call(_calldata);
+
+        if (!success) {
+            revert('SWAP_FAILED');
+        }
+        FundManager(pool).swapToAddress(
+            foundryToken,
             amountCrossMin,
             crossTargetNetwork,
             crossTargetToken,
